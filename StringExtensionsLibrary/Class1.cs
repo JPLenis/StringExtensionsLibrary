80d953d5cf927a3c26819227003bc2638484abf6
JPLenis <jlenis@my.centennialcollege.ca>
2025-04-05 20:00:51 -0400

    Add project files.

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            return !string.IsNullOrEmpty(input) && char.IsUpper(input[0]);
+        }
+    }
+}
